1abf1c705df402b331c8f62f5b3d95c8735ee1b3
AnandKumar1482 <anand.dandi9@gmail.com>
2020-03-20 17:44:00 +0530

    application.properties file is modified

--- Web/src/main/java/com/springweb/controller/Home.java
+++ Web/src/main/java/com/springweb/controller/Home.java
@@ -2,14 +2,20 @@ package com.springweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Home {
 
 	@RequestMapping("home")
-	public String home() {
-		return "home.jsp";
+	public ModelAndView home(@RequestParam("name") String myname) {
 
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("myname", myname);
+		mv.setViewName("home");
+		
+		return mv;
 	}
 
 }
